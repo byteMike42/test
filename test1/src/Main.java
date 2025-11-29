5adc68aafcce7546792a69014b94e5ab66b23a8c
byteMike42 <twdmike25@gmail.com>
2025-11-29 15:25:59 -0800

    Space Program!

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("You are cool");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Space Mission Selector\n1: Mars\n2: Jupiter\n3: Mercury\n0: Exit\nSelection: ");
+        int selection = input.nextInt();
+
+        switch(selection){
+            case 1:
+                System.out.println("Mars Rover Program ");
+                break;
+            case 2:
+                System.out.println("Jupiter Europa Program");
+                break;
+            case 3:
+                System.out.println("Mercury Probe");
+                break;
+            case 0:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Not a selected Space Program");
+                break;
+        }
 
     }
+
 }
