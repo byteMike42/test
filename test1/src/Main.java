0867079825f8028af24604e6b821c85e1f6b4166
byteMike42 <twdmike25@gmail.com>
2025-12-21 20:11:24 -0800

    Space Program!

--- test1/src/Main.java
+++ test1/src/Main.java
@@ -3,11 +3,55 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
-
         Scanner input = new Scanner(System.in);
+        String passkey = "okayboomer";
+        int attempts = 0;
+        int maxAttempts = 3;
+
+        do {
+            System.out.println("What is the password for project Astra?");
+            String guess = input.nextLine();
+            System.out.println(guess);
+            attempts++;
+            if (guess.equals(passkey)){
+                System.out.println("Access Granted");
+                break;
+            }else{
+                System.out.println("Try again");
+            }
+        }while(!input.equals(passkey) && attempts < maxAttempts);
+
+        if (!input.equals(passkey)) {
+            System.out.println("Have you forgotten your password");
+            Scanner changePassword = new Scanner(System.in);
+            String response = changePassword.nextLine();
+            System.out.printf("%s%n", response);
+            if (response.equals("yes") || response.equals("YES")) {
+                System.out.println("New passowrd: ");
+                Scanner newPassword = new Scanner(System.in);
+                String answer = newPassword.nextLine();
+                int strongPassword = 8;
+                int updatePassword = answer.length();
+                if (updatePassword > strongPassword) {
+                    System.out.println("Strong password");
+                    System.out.println(answer + "\npassword updated\nWelcome to Ad Astra");
+
+                }else{
+                    System.out.println("Weak add more characters next time");
+                }
+            } else {
+                System.out.println("okay");
+            }
+        }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.printf("Hello, %s%n", name);
 
         System.out.println("Space Mission Selector\n1: Mars\n2: Jupiter\n3: Mercury\n0: Exit\nSelection: ");
-        int selection = input.nextInt();
+        int selection = scanner.nextInt();
 
         switch(selection){
             case 1:
@@ -45,6 +89,8 @@ public class Main {
                 break;
 
         }
+
     }
+
 }
 
